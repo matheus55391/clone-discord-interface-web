10e2015c2066bfd993525f35801bb70fa9c3ad3e
matheus55391 <matheus.manulo55@gmail.com>
2022-02-08 22:50:47 -0300

    Configurando vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://matheus55391.github.io/Discord-Clone/',
   server: {
     host: true
   }
