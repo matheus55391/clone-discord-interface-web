a26f90ecaaec2c35e4259470838206685593ad3a
matheus55391 <matheus.manulo55@gmail.com>
2022-02-15 18:00:20 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,  Navigate } from 'react-router-dom';
 import { Login } from '../pages';
 import { DiscordLayout } from '../shared/layouts/DiscordLayout/index'
 export const AppRoutes = () =>{
+
     return(
         <Routes>
             
             <Route path='/Discord-Clone/Login' element={<Login/>}/>
             <Route path='/Discord-Clone/Channels' element={<DiscordLayout/>}/>
-            <Route path='/*' element={<Login/>}/>
+            <Route path='*' element={<Navigate to="/Discord-Clone/login"/>}/>
         </Routes>
     )
 }
